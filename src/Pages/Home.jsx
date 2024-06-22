56094039183c470865403f8c6cb595fbd8bfebf5
Hritika <44046963+HritikaKadam@users.noreply.github.com>
2024-06-22 12:11:52 +0530

    Add files via upload

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../Com/Hero';
+import FetchData from '../Com/FetchData';
+
+const Home = () => {
+  return <div>
+    <Hero />
+    <FetchData />
+    </div>;
+
+}
+
+export default Home
